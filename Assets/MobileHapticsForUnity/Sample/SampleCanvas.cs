e3f5d3a1a8eca4e5f8d55d6c6cc40f08ed4291fb
ulaph <ulaph4.sh@gmail.com>
2021-12-31 13:34:24 +0900

    Add haptics native plugin and sample

---
+++ Assets/MobileHapticsForUnity/Sample/SampleCanvas.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MobileHapticsForUnity.Sample
+{
+    public sealed class SampleCanvas : MonoBehaviour
+    {
+        [SerializeField] Button lightImpactButton;
+        [SerializeField] Button mediumImpactButton;
+        [SerializeField] Button heavyImpactButton;
+
+        void Start()
+        {
+            lightImpactButton.onClick.AddListener(() =>
+            {
+                Haptics.Impact(ImpactFeedbackStyle.Light);
+            });
+            
+            mediumImpactButton.onClick.AddListener(() =>
+            {
+                Haptics.Impact(ImpactFeedbackStyle.Medium);
+            });
+            
+            heavyImpactButton.onClick.AddListener(() =>
+            {
+                Haptics.Impact(ImpactFeedbackStyle.Heavy);
+            });
+        }
+    }
+}
---
+++ Assets/MobileHapticsForUnity/Scripts/AndroidVersionUtil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace MobileHapticsForUnity
+{
+    public class AndroidVersionUtil
+    {
+        public static bool IsAfterOreo
+        {
+            get
+            {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                var versionCodesClass = new AndroidJavaClass("android.os.Build$VERSION_CODES");
+                var versionClass = new AndroidJavaClass("android.os.Build$VERSION");
+                return versionClass.GetStatic<int>("SDK_INT") >= versionCodesClass.GetStatic<int>("O");
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}
